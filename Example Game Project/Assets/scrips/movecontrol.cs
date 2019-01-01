dc3d679017aebd12f8cbadee441007c5a04c939e
seongbae <ssb1004327202@daum.net>
2019-01-01 22:15:13 +0900

    add cubes

---
+++ Example Game Project/Assets/scrips/movecontrol.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movecontrol : MonoBehaviour {
+    private Vector3 b;
+    private Vector3 a;
+
+    public void OnMouseDown()
+    {
+           Vector3.Distance( a, b);
+     
+    }
+    // Use this for initialization
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
